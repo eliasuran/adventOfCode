c215c708bcd1163c85690c1b0eede64abd211f20
Elias Rissanen Uran <elura001@osloskolen.no>
2023-12-05 10:37:51 +0100

    day3 part1 ackshually done

--- 3/part1.py
+++ 3/part1.py
@@ -1,8 +1,5 @@
-# on
-# hold
-# lolol
-
 import re
+
 data = open("data.txt").readlines()
 
 dirs = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
@@ -14,18 +11,23 @@ for line in range(len(data)):
         char = data[line][x]
         if char.isdigit():
             checked = False
-            number = re.split(r'[^\d]+', data[line][x:])[0].strip("\n")
+            number = re.split(r'[^\d]+', data[line][x:])[0]
             for i, digit in enumerate(number):
                 for dir in dirs:
                     row, col = line + dir[0], x + dir[1] + i
-                    if 0 <= row < len(data) and 0 <= col < len(data[row]) and data[row][col] and not data[row][col].isdigit() and data[row][col] != ".":
+                    if (
+                        0 <= row < len(data)
+                        and 0 <= col < len(data[row])
+                        and data[row][col] != "."
+                        and not data[row][col].isdigit()
+                        and data[row][col] != "\n"
+                    ):
                         adjacent.append(int(number))
                         checked = True
                 if checked:
-                    break
+                    break  
             x += len(number)
         else:
             x += 1
 
-sum = sum(adjacent)
-print(adjacent, sum)
+print(sum(adjacent))
