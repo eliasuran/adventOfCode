7a7740644f656de86931e561cff4be6b7d7a806d
Elias Rissanen Uran <elura001@osloskolen.no>
2023-12-09 00:25:54 +0100

    dag8 part1

--- 8/part1.py
+++ 8/part1.py
@@ -2,15 +2,23 @@ data = open("data.txt").readlines()
 data = [x.strip("\n") for x in data]
 instructions = data[0]
 map = data[2:]
-found = False
 totalSteps = 0
-
-while not found:
-    step = "AAA"
+step = "AAA"
+while True:
+    if step == "ZZZ":
+        break
     for i in range(len(instructions)):
+        print(step)
+        if step == "ZZZ":
+            break
         totalSteps += 1
+        current = 0
+        for j in range(len(map)):
+            if map[j][0:3] == step:
+                current = j
+                break
         if instructions[i] == "L":
-            print("xp")
-
-
+            step = map[current][7:10]
+        else:
+            step = map[current][12:15]
 print(totalSteps)
